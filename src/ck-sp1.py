df99cc6c7e38b0df522a87f6786c0534d0eb35ae
Samuel Freitas <srfreitas97@gmail.com>
2023-03-15 15:20:33 -0300

    adicionar metricas ck

--- src/ck-sp1.py
+++ src/ck-sp1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-csv_path = 'repos.csv'
+csv_path = './data/repos.csv'
 repos_df = pd.read_csv(csv_path)
 
 
@@ -10,18 +10,18 @@ for index, row in repos_df.iterrows():
         print(row)
         print(f"Processing {row['nameWithOwner']}...")
 
-        os.system(f"git clone {row['url']}")
+        os.system(f"sudo git clone {row['url']}")
         print(row)
-        repo_path = row['nameWithOwner']
+        repo_path = row['folderName']
 
         os.system(f"sudo java -jar ./jar/ck.jar {repo_path} true 0 false ck_output")
 
-        ck_csv_path = os.path.join('ck_output', 'ck.csv')
+        ck_csv_path = os.path.join('ck_outputclass.csv')
         ck_df = pd.read_csv(ck_csv_path)
 
-        cbo_mean = ck_df['CBO'].mean()
-        dit_mean = ck_df['DIT'].mean()
-        lcom_mean = ck_df['LCOM'].mean()
+        cbo_mean = ck_df['cbo'].mean()
+        dit_mean = ck_df['dit'].mean()
+        lcom_mean = ck_df['lcom'].mean()
 
         repos_df.loc[index, 'CBO'] = cbo_mean
         repos_df.loc[index, 'DIT'] = dit_mean
