087aca3577ccbbdd0d37a1dcb86f961527545a05
Lucca Bessa <32376315+LuccaBessa@users.noreply.github.com>
2023-03-16 10:11:50 -0300

    Update ck.py

--- src/ck.py
+++ src/ck.py
@@ -1,34 +1,32 @@
 import pandas as pd
 import os
 
-csv_path = './data/repos.csv'
+csv_path = './src/data/repos.csv'
 repos_df = pd.read_csv(csv_path)
 
-if not all(col in repos_df.columns for col in ['CBO', 'DIT', 'LCOM']):
-    for index, row in repos_df.iterrows():
-        if pd.isnull(row['CBO']) or pd.isnull(row['DIT']) or pd.isnull(row['LCOM']):
-            print(f"Processing {row['name']}...")
+for index, row in repos_df.iterrows():
+    if pd.isna(row['CBO']) or pd.isna(row['DIT']) or pd.isna(row['LCOM']):
+        print(f"Processing {row['folderName']}...")
 
-            os.system(f"git clone {row['url']}")
-            repo_path = row['name']
+        os.system(f"git clone {row['url']}")
+        repo_path = row['folderName']
 
-            os.system(f"sudo java -jar ./jar/ck.jar {repo_path} true 0 false ck_output")
+        os.system(f"sudo java -jar ./src/jar/ck.jar {repo_path} true 0 false ck_output")
 
-            ck_csv_path = os.path.join('ck_outputclass.csv')
-            ck_df = pd.read_csv(ck_csv_path)
+        ck_csv_path = "./ck_outputclass.csv"
+        ck_df = pd.read_csv(ck_csv_path)
 
-            cbo_mean = ck_df['CBO'].median()
-            dit_mean = ck_df['DIT'].max()
-            lcom_mean = ck_df['LCOM'].median()
+        cbo_mean = ck_df['cbo'].median()
+        dit_mean = ck_df['dit'].max()
+        lcom_mean = ck_df['lcom'].median()
 
-            repos_df.loc[index, 'CBO'] = cbo_mean
-            repos_df.loc[index, 'DIT'] = dit_mean
-            repos_df.loc[index, 'LCOM'] = lcom_mean
-            repos_df.to_csv(csv_path, index=False)
+        repos_df.loc[index, 'CBO'] = cbo_mean
+        repos_df.loc[index, 'DIT'] = dit_mean
+        repos_df.loc[index, 'LCOM'] = lcom_mean
+        repos_df.to_csv(csv_path, index=False)
 
-            os.system(f"rm -rf {repo_path}")
-            os.system(f"rm ck_outputclass.csv")
-            os.system(f"rm ck_outputmethod.csv")
-
-else:
-    print("Required columns already exist in the CSV file.")
+        os.system(f"rm -rf {repo_path}")
+        os.system(f"rm -f ck_outputclass.csv")
+        os.system(f"rm -f ck_outputmethod.csv")
+    else:
+        print(f"{row['folderName']} already processed")
